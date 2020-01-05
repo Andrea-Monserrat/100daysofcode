09ef678c0992b892b091c575c8663b1c08796295
Andrea Monserrat <andrea.monserrat.ar@gmail.com>
2020-01-05 16:01:34 -0600

    Update config.py

--- tweet-bots/bots/config.py
+++ tweet-bots/bots/config.py
@@ -5,14 +5,10 @@ import os
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = os.getenv("yOFrLYJZ9hyTJtQT3NxbT2yUK5")
-    consumer_secret = os.getenv("0RbjOnqEBnMbKjGfgDYeWulqBhipShe35i4q3S55Nq2J5TBvIF5")
-    access_token = os.getenv("1064994930404274178-DbqZy5BwUJCshvLuuSFaYE7GHpiYbA5")
-    access_token_secret = os.getenv("68k5WeFwKBzCm2dsnVodR3Z1JTEC1wnugDCWOph1hwh6o5")
-    #consumer_key = "yOFrLYJZ9hyTJtQT3NxbT2yUK5" #APIkey
-    #consumer_secret = "0RbjOnqEBnMbKjGfgDYeWulqBhipShe35i4q3S55Nq2J5TBvIF5"#Apisecrectkey
-    #access_token="1064994930404274178-DbqZy5BwUJCshvLuuSFaYE7GHpiYbA5"
-    #access_token_secret = "68k5WeFwKBzCm2dsnVodR3Z1JTEC1wnugDCWOph1hwh6o5"
+    consumer_key = os.getenv("")
+    consumer_secret = os.getenv("")
+    access_token = os.getenv("")
+    access_token_secret = os.getenv("6")
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True,
